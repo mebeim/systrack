ef3a4a538a9b1be940cf50a27f7df33af1b4e92c
Marco Bonelli <marco@mebeim.net>
2025-03-27 15:40:53 +0100

    arm64: Implement _dummy_syscall_code() helper

--- src/systrack/arch/arm64.py
+++ src/systrack/arch/arm64.py
@@ -2,6 +2,7 @@ from typing import Tuple, List, Optional
 
 from ..elf import Symbol, ELF, E_MACHINE
 from ..kconfig_options import VERSION_INF
+from ..syscall import Syscall
 from ..type_hints import KernelVersion
 from ..utils import VersionedDict, noprefix
 
@@ -79,3 +80,33 @@ class ArchArm64(Arch):
 		# E.g. v5.18 COMPAT_SYSCALL_DEFINE6(aarch32_mmap2, ...)
 		# E.g. v5.2-v6.13+ SYSCALL_DEFINE1(arm64_personality, ...)
 		return noprefix(name, 'aarch32_', 'arm64_')
+
+	def _dummy_syscall_code(self, sc: Syscall, vmlinux: ELF) -> Optional[bytes]:
+		# Match the following code exactly with either -22 (EINVAL) or -38
+		# (-ENOSYS) as immediate for MOV:
+		#
+		#     928004a0    mov  x0, #0xffffffffffffffda // #-38
+		#     d65f03c0    ret
+		#
+		# Taken from __arm64_sys_pkey_alloc on v6.11.
+		#
+		if sc.symbol.size > 8 or sc.symbol.size == 4:
+			return None
+
+		assert not vmlinux.big_endian
+
+		code = vmlinux.read_symbol(sc.symbol)
+		if not code.endswith(b'\xc0\x03\x5f\xd6'): # ret
+			return None
+
+		# MOVN <Xd>, #<imm>{, LSL #<shift>}
+		mov = int.from_bytes(code[:4], 'little')
+		if mov & 0xff80001f != 0x92800000:
+			return None
+
+		hw = (mov >> 20) & 0x3
+		imm = ~(((mov >> 5) & 0xffff) << (hw * 16))
+		if imm == -38 or imm == -22:
+			return code
+
+		return None
